b3d8915f6d0734a949923a2f162c8aba734c7f2a
bishir-tm <bishirtijjani247@gmail.com>
2024-10-23 18:18:59 +0100

    change db name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "buzzer_system";
+$dbname = "bistij_buzzer_system";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
